8a1121466e91aafc5ca4744a4597bc9d53cda7a1
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-09 20:32:19 +0530

    set host

--- set-up-host.sh
+++ set-up-host.sh
@@ -9,5 +9,8 @@ hostnamectl set-hostname "$1"
 # Update the hostname part of Host File
 echo "`hostname -I | awk '{ print $1 }'` `hostname`" >> /etc/hosts
 
+# Refresh
+/bin/bash
+
 # Back to normal user
 exit
