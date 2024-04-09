2ae71867ed7d0ba2de629f5702beddb6a8556a5c
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-09 20:30:44 +0530

    set host

--- set-up-host.sh
+++ set-up-host.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Setup Hostname
-sudo hostnamectl set-hostname "$1"
+hostnamectl set-hostname "$1"
 
 # Refresh
 /bin/bash
-
-# switch to root
-sudo su -
 
 # Update the hostname part of Host File
 echo "`hostname -I | awk '{ print $1 }'` `hostname`" >> /etc/hosts
