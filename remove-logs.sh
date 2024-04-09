31baaf2dbe9c94ffb7c9a434911a45459792d307
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-09 21:36:19 +0530

    log

--- remove-logs.sh
+++ remove-logs.sh
@@ -7,10 +7,19 @@
 # Purpose: Remove logs morethan 14days
 ################################################
 
-SOURCE_DIRC="/tmp/sample-logs"
+SOURCE_DIRC=$1
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-while read -r line
+
+if [ ! -d "$SOURCE_DIRC" ]; then
+  echo -e "$R ERROR:: $SOURCE_DIRC does exist.$N"
+fi
+
+while IFS= read -r line
 do 
-    echo "Deleting the file .. $line"
-    rm $line
+    echo "Deleting the file .. $R $line $N"
+    rm -rf $line
 done <<< $(find $SOURCE_DIRC -mtime +14 -type f -name '*.log')
