decd997d28247699518530189966a1196b73de5c
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-09 21:17:36 +0530

    log

---
+++ log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############################################
+# Author: ROYAL REDDY
+# Date: 09-04
+# Version: V1
+# Purpose: Remove logs morethan 14days
+################################################
+
+SOURCE_DIRC="/tmp/sample-logs"
+
+while read -r line
+do 
+    echo "$line"
+done <<< $(find $SOURCE_DIRC -mtime +14 -type f -name '*.log')
